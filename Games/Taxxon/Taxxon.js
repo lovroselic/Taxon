c3d76c0c22a99493edb9fb541d4a0e5fcf399a32
Lovro Selic <lovro.selic@gmail.com>
2026-04-08 12:32:50 +0200

    0.8.5

--- Games/Taxxon/Taxxon.js
+++ Games/Taxxon/Taxxon.js
@@ -207,7 +207,7 @@ const INI = {
     SUN_HEIGHT_FACTOR: 7.5, //7.5
     CREEP_SPEED: 1.8,
     PAD_BETWEEN_LEVELS: 5,
-    LAST_LEVEL: 11,
+    LAST_LEVEL: 12,
     SIDE_SPEED: 5.0,
     FALL_SPEED: 5.0,
     SHIT_ROT_ANGLE: Math.radians(30),
@@ -218,7 +218,7 @@ const INI = {
 };
 
 const PRG = {
-    VERSION: "0.8.4",
+    VERSION: "0.8.5",
     NAME: "TaXXon",
     YEAR: "2026",
     SG: "TAXXON",
@@ -533,7 +533,7 @@ const GAME = {
         GAME.extraLife = SCORE.extraLife.clone();
         GAME.lives = 3;
         //GAME.level = 1;
-        GAME.level = 11;
+        GAME.level = 12;
         GAME.score = 0;
         //GAME.score = 9990;
 
